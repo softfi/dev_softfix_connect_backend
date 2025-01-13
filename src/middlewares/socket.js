c4c8e957d6bff6ca2b4c74805c3c5bb6928cc504
Aman <aman.softfix@gmail.com>
2025-01-13 12:08:05 +0530

    middleware log

--- src/middlewares/socket.js
+++ src/middlewares/socket.js
@@ -12,6 +12,10 @@ export const socketAuthentication = (socket, next) => {
       return;
     }
     let token = null;
+console.log("-----------");
+console.log(authHandshake?.headers);
+console.log("-----------");
+console.log(APP_ENV);
 
     if (APP_ENV === "development") {
       token = authHandshake?.headers["authorization"];
