061b51e70924338758ac8dd72c11aad8dc6242c0
Aman <aman.softfix@gmail.com>
2025-01-23 16:54:17 +0530

    ssl implemented

--- src/index.js
+++ src/index.js
@@ -59,31 +59,31 @@ app.all("*", (req, res) => {
   return res.status(404).send({ status: false, msg: "Not Found" });
 });
 
-// const httpServer = http.createServer(app);
+const httpServer = http.createServer(app);
 
 /*********************
     SSL CERTIFICATE 
 **********************/
 
-// const privateKey = readFileSync("./ssl/credentials/privatekey.pem", "utf8");
-// const certificate = readFileSync("./ssl/credentials/cert.pem", "utf8");
-// const chain = readFileSync("./ssl/credentials/chain.pem", "utf8");
+const privateKey = readFileSync("./ssl/privateKey.pem", "utf8");
+const certificate = readFileSync("./ssl/cert.pem", "utf8");
+const chain = readFileSync("./ssl/chain.pem", "utf8");
 
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: chain,
-// };
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: chain,
+};
 
-// let serverInstance = null;
+let serverInstance = null;
 
-// if (APP_ENV === "production") {
-//   serverInstance = https.createServer(credentials, app);
-// } else {
-//   serverInstance = http.createServer(app);
-// }
+if (APP_ENV === "production" || APP_ENV === "development") {
+  serverInstance = https.createServer(credentials, app);
+} else {
+  serverInstance = http.createServer(app);
+}
 
-let serverInstance = http.createServer(app);
+// let serverInstance = http.createServer(app);
 
 /***************************
           SOCKET 
